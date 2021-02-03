bbe81555c77b1cd73dab84ee6dd9d42e28faf587
Kaden <cartwrightkaden@gmail.com>
2021-02-02 20:37:54 -0600

    finished basic JWT implementation

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,16 @@
 import Express from "express";
 import dotenv from 'dotenv';
 import {connectDB,disconnectDB} from "./database"
-import router from './routes/router'
+import router,{openRoutes} from './routes/router'
+import jwt from 'express-jwt'
 dotenv.config()
 const PORT = process.env.PORT
 const app = Express()
 app.use([Express.json(),Express.urlencoded({extended:true})])//parse url and json data
-
+app.use(jwt({
+    secret: process.env.JWT_SECRET,
+    algorithms: ['HS256']
+}).unless({path:[...openRoutes]}))
 app.use('/',router)
 
 app.listen(PORT,async ()=>{
--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,6 +2,7 @@ import { Request,Response } from 'express';
 import {Container} from 'typedi'
 import {check,ValidationChain} from 'express-validator'
 import AuthService, {ILogin} from '../services/AuthService';
+import jwt from 'jsonwebtoken'
 /**
  * the structure of this controller is such that any function that depends on validation of parameters should define BOTH 
  *      a handler function 
@@ -11,9 +12,15 @@ import AuthService, {ILogin} from '../services/AuthService';
 const loginHandler:(req:Request,res:Response)=>void = async(req:Request,res:Response)=>{
     const authService:AuthService = Container.get(AuthService)
     const creds:ILogin = {email:req.body.email,password: req.body.password}
-    let user =(await authService.login(creds)).toJSON()
-        let userFiltered =(({name,email})=>({name,email}))(user)
-    res.json({userFiltered})
+    let userDoc =(await authService.login(creds))
+    if(!userDoc) {res.status(404).send()}
+    let user = userDoc.toJSON()
+    let userFiltered =(({name,email})=>({name,email}))(user)
+    let token = jwt.sign({
+        ...userFiltered,
+        permissions:['admin']
+    },process.env.JWT_SECRET,{expiresIn:'1000d'})
+    res.json({token:token})
     //need to set token or cookie
 }
 const loginValidator:Array<ValidationChain>=[
